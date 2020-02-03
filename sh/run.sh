93d5d283c9a5959ddc94b78b5fe01c7ca41a8c80
stanislawbartowski@gmail.com <stanislawbartkowski@gmail.com>
2020-02-03 19:39:42 +0100

    Add run.sh

---
+++ sh/run.sh
@@ -0,0 +1,56 @@
+. ./source.rc
+HJAR=../target/hbasespark.jar
+# java -Dlog4j.configuration=file:$PWD/log4j.properties  -cp $LIB:$HJAR HelloScala ./param.properties sparktest
+
+PARAM=./param.properties
+
+help() {
+    echo "./run.sh <param>"
+    echo " sparktest : Simple wordcount spark test"
+    echo " loadhbase : Load textfile into hbase"
+    echo " sparkhbase : Run hbase spark job"
+    exit 
+}
+
+readparam() {
+  while IFS='=' read -r key value
+  do
+    case $key in 
+      infile) INFILE=$value;;
+      outfile) OUTFILE=$value;;
+      *);;
+    esac  
+  done < $PARAM
+#  echo $INFILE
+#  echo $OUTFILE
+}
+
+removetemp() {
+  hdfs dfs -rm -r $OUTFILE
+}
+
+runsparktest() {  
+  removetemp
+  local -r dire=`dirname $INFILE`
+  hdfs dfs -mkdir -p $dire
+  hdfs dfs -copyFromLocal $INFILE $INFILE
+  spark-submit  --class HelloScala --master yarn --name ShakespeareWordCount $HJAR $PARAM sparktest    
+}
+
+runloadhbase() {
+  java -Dlog4j.configuration=file:$PWD/log4j.properties  -cp $CONF:$LIB:$HJAR HelloScala $PARAM hbaseload
+}
+
+runsparkhbase() {
+  removetemp
+  spark-submit --conf "spark.driver.extraClassPath=$CONF:$LIB" --class HelloScala --master yarn $HJAR $PARAM hbasespark
+}
+
+readparam
+
+case $1 in 
+  sparktest) runsparktest;;
+  loadhbase) runloadhbase;;
+  sparkhbase) runsparkhbase;;
+  *) help;;
+esac
